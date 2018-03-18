899390d7c07c63143e6dbdfa4c39d49df0f11c19
Bryon Urbanec <bryonurbanec@Bryons-MacBook-Pro.local>
2018-03-18 17:59:35 -0600

    Made some changes and added some defualts

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+  color();
+});
+
 // get value of text input
 function color() {
   const colorHex = document.getElementById('color').value;
   const splotches = document.getElementById('splotchNum').value;
   const paletteWrapper = document.querySelector('colorGradient');
 
-  // document.getElementById('colorGradient').innerHTML = colorHex;
-
   for( let i = 0; i < splotches; i++) {
     const div = document.createElement('div');
     div.className = 'splotch';
-    div.style.backgroundColor = colorHex;
+    div.style.backgroundColor = colorHex || colorPicker;
     document.getElementById('colorGradient').appendChild(div);
   }
-  console.log(splotches);
 }
+
+const inputs = document.querySelectorAll('.controls input');
+inputs.forEach(input => input.addEventListener('change', color));
